c62c0e4885c0e422110c93a7f96f35d2ddcb8bb8
luizbottino <luizbottino@outlook.com>
2023-06-07 11:15:28 -0300

    Alteração da Model do Usuario

--- ViewModels/UsuarioViewModel.cs
+++ ViewModels/UsuarioViewModel.cs
@@ -10,7 +10,6 @@ namespace ViewModels
         public int Id { get; set; }
         public string UserName { get; set; }
         public string Role { get; set; }
-        public int IdUnidade { get; set; }
         public DateTime DataCadastro { get; set; }
     }
 
